f1060512129713e95d22d4cf9c3fcb6795c0fc7d
Akira Nonaka <nonaka@mac.com>
2016-04-04 18:30:52 +0900

    WIP, more tests added.

--- src/XoxzoClient.php
+++ src/XoxzoClient.php
@@ -1,33 +1,45 @@
 <?php
 
 namespace xoxzo\cloudphp;
+require 'vendor/autoload.php';
+
+use \GuzzleHttp\Client as HttpClient;
 
 class XoxzoClient {
   private $sid;
   private $auth_token;
 
   function __construct($sid, $auth_token){
+
     $this->sid = $sid;
     $this->auth_token = $auth_token;
-    $api_host = getenv('XOXZO_API_HOST');
-
-    if ($api_host == "") {
-      $api_host = "https://api.xoxzo.com";
-    }
+    $this->basic_auth_data = ['auth' => [ $sid, $auth_token]];
+    $this->client = new HttpClient();
+    $api_host = "https://api.xoxzo.com";
 
     $this->xoxzo_api_sms_url = $api_host . "/sms/messages/";
     $this->xoxzo_api_voice_simple_url =  $api_host . "/voice/simple/playback/";
-    echo "\n";
-    echo $this->sid,"\n";
-    echo $this->auth_token,"\n";
-    echo $this->xoxzo_api_sms_url,"\n";
-    echo $this->xoxzo_api_voice_simple_url,"\n";
   }
 
   public function send_sms($message, $recipient, $sender){
+    $params = ['form_params' => [
+      'message' => $message,
+      'recipient' => $recipient,
+      'sender' => $sender
+    ]];
+
+    $resp = $this->client->post(
+      $this->xoxzo_api_sms_url,
+      $this->basic_auth_data + $params);
+    echo $resp;
   }
 
   public function get_sms_delivery_status($msgid){
+    $url = $this->xoxzo_api_sms_url . $msgid;
+    $resp = $this->client->get(
+      $url,
+      $this->basic_auth_data);
+    return json_decode($resp->getBody());
   }
 
   public function call_simple_playback($caller, $recipient, $recording_url){
@@ -37,6 +49,4 @@ class XoxzoClient {
   }
 }
 
-# test code
-$xc = new XoxzoClient("", "");
 ?>
--- tests/XoxzoClientTest.php
+++ tests/XoxzoClientTest.php
@@ -7,10 +7,36 @@ use xoxzo\cloudphp\XoxzoClient;
 
 class XoxzoClientTest extends \PHPUnit_Framework_TestCase {
 
-  public function test01() {
+  public function setUp(){
     $sid = getenv('XOXZO_API_SID');
     $auth_token = getenv('XOXZO_API_AUTH_TOKEN');
-    $xc = new XoxzoClient($sid,$auth_token);
+    $this->xc = new XoxzoClient($sid,$auth_token);
+  }
+
+  public function test_send_sms_01() {
+    # $this->xc->send_sms("Hello form Xoxzo PHP","+818054695209","814512345678");
+  }
+
+  public function test_get_sms_delivery_status_01() {
+    # good msgid
+    $resp = $this->xc->get_sms_delivery_status("W0kYZfyBeTpqcPv2AnKolSjOwDr3d87h");
+    $this->assertObjectHasAttribute('msgid', $resp);
+  }
+
+  /**
+   * @expectedException     GuzzleHttp\Exception\ClientException
+   * @expectedExceptionCode 404
+  */
+
+  public function test_get_sms_delivery_status_02() {
+    # bad msgid
+    $resp =  $this->xc->get_sms_delivery_status("W0kYZfyBeTpqcPv2AnKolSjOwDr3d87i");
+  }
+
+  public function test_get_sms_delivery_status_03() {
+    # retreave all msgids
+    $resp =  $this->xc->get_sms_delivery_status("");
+    $this->assertTrue(is_array($resp));
   }
 }
 ?>
