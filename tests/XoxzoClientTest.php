372647fde4910fde02ac2c0b9de2acdda5c662c1
Akira Nonaka <nonaka@mac.com>
2016-04-27 10:00:23 +0900

    set tab length 4

--- tests/XoxzoClientTest.php
+++ tests/XoxzoClientTest.php
@@ -1,105 +1,117 @@
 <?php
-
 namespace xoxzo\cloudphp\tests;
-
 require_once 'src/XoxzoClient.php';
+
 use xoxzo\cloudphp\XoxzoClient;
+class XoxzoClientTest extends \PHPUnit_Framework_TestCase
 
-class XoxzoClientTest extends \PHPUnit_Framework_TestCase {
-
-  public function setUp(){
-    $sid = getenv('XOXZO_API_SID');
-    $auth_token = getenv('XOXZO_API_AUTH_TOKEN');
-    $this->xc = new XoxzoClient($sid,$auth_token);
-  }
-
-  public function test_send_sms_success01() {
-    $this->markTestIncomplete('Skip this test for now.');
-    $recipient = getenv('XOXZO_API_TEST_RECIPIENT');
-    $resp = $this->xc->send_sms("Hello form Xoxzo PHP lib",$recipient,"814512345678");
-    $this->assertEquals($resp->errors,null);
-    $this->assertObjectHasAttribute('msgid', $resp->messages[0]);
-
-    $msgid = $resp->messages[0]->msgid;
-    $resp = $this->xc->get_sms_delivery_status($msgid);
-    $this->assertEquals($resp->errors, null);
-    $this->assertObjectHasAttribute('msgid', $resp->messages);
-  }
-
-  public function test_send_sms_fail01() {
-    # bad recepient
-    $resp = $this->xc->send_sms("Hello form Xoxzo PHP lib","+8108012345678","814512345678");
-    $this->assertEquals($resp->errors, 400);
-    $this->assertObjectHasAttribute('recipient', $resp->messages);
+{
+    public function setUp()
+    {
+        $sid = getenv('XOXZO_API_SID');
+        $auth_token = getenv('XOXZO_API_AUTH_TOKEN');
+        $this->xc = new XoxzoClient($sid, $auth_token);
     }
 
-  public function test_get_sms_delivery_status_01() {
-    # bad msgid
-    $resp =  $this->xc->get_sms_delivery_status("W0kYZfyBeTpqcPv2AnKolSjOwDr3d87i-xxx");
-    $this->assertEquals($resp->errors, 404);
-    # this test currently fails due to bug
-    #$this->assertEquals($resp->messages, null);
-    # todo: fix this test when the bug is fixed
-    $this->assertEquals($resp->messages, []);
-  }
+    public function test_send_sms_success01()
+    {
+        $this->markTestIncomplete('Skip this test for now.');
+        $recipient = getenv('XOXZO_API_TEST_RECIPIENT');
+        $resp = $this->xc->send_sms("Hello form Xoxzo PHP lib", $recipient, "814512345678");
+        $this->assertEquals($resp->errors, null);
+        $this->assertObjectHasAttribute('msgid', $resp->messages[0]);
+        $msgid = $resp->messages[0]->msgid;
+        $resp = $this->xc->get_sms_delivery_status($msgid);
+        $this->assertEquals($resp->errors, null);
+        $this->assertObjectHasAttribute('msgid', $resp->messages);
+    }
 
-  public function test_get_sms_delivery_status_02() {
-    # retreave all msgids
-    $resp =  $this->xc->get_sms_delivery_status("");
-    $this->assertEquals($resp->errors, null);
-    $this->assertObjectHasAttribute('msgid', $resp->messages[0]);
-  }
+    public function test_send_sms_fail01()
+    {
+        // bad recepient
+        $resp = $this->xc->send_sms("Hello form Xoxzo PHP lib", "+8108012345678", "814512345678");
+        $this->assertEquals($resp->errors, 400);
+        $this->assertObjectHasAttribute('recipient', $resp->messages);
+    }
 
-  public function test_get_sent_sms_list_01(){
-    $resp =  $this->xc->get_sent_sms_list("");
-    $this->assertEquals($resp->errors, null);
-    $this->assertObjectHasAttribute('msgid', $resp->messages[0]);
-  }
+    public function test_get_sms_delivery_status_01()
+    {
+        // bad msgid
+        $resp = $this->xc->get_sms_delivery_status("W0kYZfyBeTpqcPv2AnKolSjOwDr3d87i-xxx");
+        $this->assertEquals($resp->errors, 404);
 
-  public function test_get_sent_sms_list_02(){
-    $resp =  $this->xc->get_sent_sms_list('>=2016-04-01');
-    $this->assertEquals($resp->errors, null);
-    $this->assertObjectHasAttribute('msgid', $resp->messages[0]);
-  }
+        // this test currently fails due to bug
+        // $this->assertEquals($resp->messages, null);
+        // todo: fix this test when the bug is fixed
 
-  public function test_get_sent_sms_list_03(){
-    $resp =  $this->xc->get_sent_sms_list('>=2016-13-01');
-    $this->assertEquals($resp->errors, 400);
-    $this->assertObjectHasAttribute('sent_date', $resp->messages);
-  }
+        $this->assertEquals($resp->messages, []);
+    }
 
-  public function test_call_simple_playback_success01() {
-    $this->markTestIncomplete('Skip this test for now.');
-    $recipient = getenv('XOXZO_API_TEST_RECIPIENT');
-    $recording_url = getenv('XOXZO_API_TEST_MP3');
-    $caller = '814512345678';
-    $resp = $this->xc->call_simple_playback($caller, $recipient, $recording_url);
-    $this->assertEquals($resp->errors, null);
-    $this->assertObjectHasAttribute('callid', $resp->messages[0]);
+    public function test_get_sms_delivery_status_02()
+    {
+        // retreave all msgids
+        $resp = $this->xc->get_sms_delivery_status("");
+        $this->assertEquals($resp->errors, null);
+        $this->assertObjectHasAttribute('msgid', $resp->messages[0]);
+    }
 
-    $callid = $resp->messages[0]->callid;
-    $resp = $this->xc->get_simple_playback_status($callid);
-    $this->assertEquals($resp->errors, null);
-    $this->assertObjectHasAttribute('callid', $resp->messages);
-  }
+    public function test_get_sent_sms_list_01()
+    {
+        $resp = $this->xc->get_sent_sms_list("");
+        $this->assertEquals($resp->errors, null);
+        $this->assertObjectHasAttribute('msgid', $resp->messages[0]);
+    }
 
-  public function test_call_simple_playback_fail01() {
-    $recipient = '+8108012345678';
-    $recording_url = getenv('XOXZO_API_TEST_MP3');
-    $caller = '814512345678';
-    $resp = $this->xc->call_simple_playback($caller, $recipient, $recording_url);
-    $this->assertEquals($resp->errors, 400);
-    $this->assertObjectHasAttribute('recipient', $resp->messages);
-  }
+    public function test_get_sent_sms_list_02()
+    {
+        $resp = $this->xc->get_sent_sms_list('>=2016-04-01');
+        $this->assertEquals($resp->errors, null);
+        $this->assertObjectHasAttribute('msgid', $resp->messages[0]);
+    }
 
-  public function test_get_simple_playback_status_01() {
-    # bad call id
-    $resp =  $this->xc->get_simple_playback_status("b160f404-f1b8-4576-b56a-f557c3fca483");
-    $this->assertEquals($resp->errors, 404);
-    # this test currently fails due to bug
-    #$this->assertEquals($resp->messages, null);
-    # todo: fix this test when the bug is fixed
-    $this->assertEquals($resp->messages, "");
-  }
+    public function test_get_sent_sms_list_03()
+    {
+        $resp = $this->xc->get_sent_sms_list('>=2016-13-01');
+        $this->assertEquals($resp->errors, 400);
+        $this->assertObjectHasAttribute('sent_date', $resp->messages);
+    }
+
+    public function test_call_simple_playback_success01()
+    {
+        $this->markTestIncomplete('Skip this test for now.');
+        $recipient = getenv('XOXZO_API_TEST_RECIPIENT');
+        $recording_url = getenv('XOXZO_API_TEST_MP3');
+        $caller = '814512345678';
+        $resp = $this->xc->call_simple_playback($caller, $recipient, $recording_url);
+        $this->assertEquals($resp->errors, null);
+        $this->assertObjectHasAttribute('callid', $resp->messages[0]);
+        $callid = $resp->messages[0]->callid;
+        $resp = $this->xc->get_simple_playback_status($callid);
+        $this->assertEquals($resp->errors, null);
+        $this->assertObjectHasAttribute('callid', $resp->messages);
+    }
+
+    public function test_call_simple_playback_fail01()
+    {
+        $recipient = '+8108012345678';
+        $recording_url = getenv('XOXZO_API_TEST_MP3');
+        $caller = '814512345678';
+        $resp = $this->xc->call_simple_playback($caller, $recipient, $recording_url);
+        $this->assertEquals($resp->errors, 400);
+        $this->assertObjectHasAttribute('recipient', $resp->messages);
+    }
+
+    public function test_get_simple_playback_status_01()
+    {
+        // bad call id
+        $resp = $this->xc->get_simple_playback_status("b160f404-f1b8-4576-b56a-f557c3fca483");
+        $this->assertEquals($resp->errors, 404);
+
+        // this test currently fails due to bug
+        // $this->assertEquals($resp->messages, null);
+        // todo: fix this test when the bug is fixed
+
+        $this->assertEquals($resp->messages, "");
+    }
 }
 ?>
